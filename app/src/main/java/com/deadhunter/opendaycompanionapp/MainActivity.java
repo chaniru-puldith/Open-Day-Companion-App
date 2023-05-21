f99e16c415ee45a34a0fddf6477918896ab4f1c1
Sachini <sachinirathnayake84@gmail.com>
2023-05-21 14:25:30 +0530

    Notification-Issue fixed

--- app/src/main/java/com/deadhunter/opendaycompanionapp/MainActivity.java
+++ app/src/main/java/com/deadhunter/opendaycompanionapp/MainActivity.java
@@ -3,7 +3,12 @@ package com.deadhunter.opendaycompanionapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -15,6 +20,7 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 
@@ -24,7 +30,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel1();
 
+        // Create intent and pending intent for Event 1
+        Intent intent1 = new Intent(MainActivity.this, ReminderBroadcast.class);
+        intent1.putExtra("event_title", "Event 1");
+        intent1.putExtra("event_time", "10.00AM");
+        intent1.putExtra("notificationId", 1);
+        PendingIntent pendingIntent1 = PendingIntent.getBroadcast(MainActivity.this, 123, intent1, PendingIntent.FLAG_IMMUTABLE);
+
+        // Set alarm for Event 1
+        AlarmManager alarmManager1 = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.set(Calendar.YEAR, 2023);
+        calendar1.set(Calendar.MONTH, Calendar.MAY);
+        calendar1.set(Calendar.DAY_OF_MONTH, 11);
+        calendar1.set(Calendar.HOUR_OF_DAY, 00);
+        calendar1.set(Calendar.MINUTE, 25);
+        calendar1.set(Calendar.SECOND, 0);
+        long event1TimeInMillis = calendar1.getTimeInMillis();
+        if (event1TimeInMillis > System.currentTimeMillis()) {
+            alarmManager1.set(AlarmManager.RTC_WAKEUP, event1TimeInMillis, pendingIntent1);
+        }
+
+        // Create intent and pending intent for Event 2
+        Intent intent2 = new Intent(MainActivity.this, ReminderBroadcast.class);
+        intent2.putExtra("event_title", "Event 2");
+        intent2.putExtra("event_time", "10.30AM");
+        intent2.putExtra("notificationId", 2);
+        PendingIntent pendingIntent2 = PendingIntent.getBroadcast(MainActivity.this, 124, intent2, PendingIntent.FLAG_IMMUTABLE);
+
+        // Set alarm for Event 2
+        AlarmManager alarmManager2 = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.set(Calendar.YEAR, 2023);
+        calendar2.set(Calendar.MONTH, Calendar.MAY);
+        calendar2.set(Calendar.DAY_OF_MONTH, 11);
+        calendar2.set(Calendar.HOUR_OF_DAY, 00);
+        calendar2.set(Calendar.MINUTE, 22);
+        calendar2.set(Calendar.SECOND, 0);
+        long event2TimeInMillis = calendar2.getTimeInMillis();
+        if (event2TimeInMillis > System.currentTimeMillis()) {
+            alarmManager2.set(AlarmManager.RTC_WAKEUP, event2TimeInMillis, pendingIntent2);
+        }
+
+        // Create intent and pending intent for Event 3
+        Intent intent3 = new Intent(MainActivity.this, ReminderBroadcast.class);
+        intent3.putExtra("event_title", "Event 3");
+        intent3.putExtra("event_time", "11.00AM");
+        intent3.putExtra("notificationId", 3);
+        PendingIntent pendingIntent3 = PendingIntent.getBroadcast(MainActivity.this, 125, intent3, PendingIntent.FLAG_IMMUTABLE);
+
+        // Set alarm for Event 3
+        AlarmManager alarmManager3 = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Calendar calendar3 = Calendar.getInstance();
+        calendar3.set(Calendar.YEAR, 2023);
+        calendar3.set(Calendar.MONTH, Calendar.MAY);
+        calendar3.set(Calendar.DAY_OF_MONTH, 11);
+        calendar3.set(Calendar.HOUR_OF_DAY, 00);
+        calendar3.set(Calendar.MINUTE, 21);
+        calendar3.set(Calendar.SECOND, 0);
+        long event3TimeInMillis = calendar3.getTimeInMillis();
+        if (event3TimeInMillis > System.currentTimeMillis()) {
+            alarmManager3.set(AlarmManager.RTC_WAKEUP, event3TimeInMillis, pendingIntent3);
+        }
         // Setting up slideshow on main menu
         ImageSlider imageSlider = findViewById(R.id.slider);
 
@@ -65,6 +134,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private void createNotificationChannel1() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name1 = "Event1";
+            String description1 = "Main Event 1";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel1 = new NotificationChannel("CINECOD1", name1, importance);
+            channel1.setDescription(description1);
+
+            NotificationManager notificationManager1 = getSystemService(NotificationManager.class);
+            notificationManager1.createNotificationChannel(channel1);
+
+        }
+    }
 
     // Declaring what should be happened after clicking each CardView
     @Override
