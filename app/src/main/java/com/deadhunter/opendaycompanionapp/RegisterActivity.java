72a3b58a99916ead282a97e35ca6a352c62ebd27
Chaniru <C.P.AluthgamaArachchigeDon@wlv.ac.uk>
2023-05-22 13:59:17 +0530

    Register Now Issue Fixed

--- app/src/main/java/com/deadhunter/opendaycompanionapp/RegisterActivity.java
+++ app/src/main/java/com/deadhunter/opendaycompanionapp/RegisterActivity.java
@@ -43,18 +43,6 @@ public class RegisterActivity extends AppCompatActivity {
     Button regBtn;
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
