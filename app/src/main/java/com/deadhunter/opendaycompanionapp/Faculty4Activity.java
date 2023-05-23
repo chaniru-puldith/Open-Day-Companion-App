ff5ea8a26045ad4c9eac45893a0b4ffef1de292f
Laviru Herath <L.G.Herath@wlv.ac.uk>
2023-05-23 13:41:32 +0530

    Campus Roadmap

--- app/src/main/java/com/deadhunter/opendaycompanionapp/Faculty4Activity.java
+++ app/src/main/java/com/deadhunter/opendaycompanionapp/Faculty4Activity.java
@@ -2,13 +2,36 @@ package com.deadhunter.opendaycompanionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Faculty4Activity extends AppCompatActivity {
 
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty4);
+        videoView = findViewById(R.id.video);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName()+"/" + R.raw.management));
+        videoView.start();
+
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.management);
+        videoView.setVideoURI(videoUri);
+
+        // Autoplay the video
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+                mediaPlayer.start();
+            }
+        });
     }
 }
