3dcec5bcde5c64545e919ac2159163cbc3266773
Chaniru <C.P.AluthgamaArachchigeDon@wlv.ac.uk>
2023-05-23 20:17:49 +0530

    Login Bug Fix

--- app/src/main/java/com/deadhunter/opendaycompanionapp/LoginActivity.java
+++ app/src/main/java/com/deadhunter/opendaycompanionapp/LoginActivity.java
@@ -23,17 +23,6 @@ public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
     FirebaseAuth mAuth;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
